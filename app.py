0559d24a1631d9486e0191279c0005f178114f01
cloudeng24 <cloudeng65@gmail.com>
2025-07-22 07:07:18 +0530

    app.yudated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Cloud Run with Python!"
+    return "Hello from Cloud Run with Python! and We have done the oidc interation as well!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
